305bc62d9b11d703bf9184464ffef007a029383d
xxingyiyuan <1391065515@qq.com>
2023-11-19 19:53:09 +0800

    feat: 数据

---
+++ taskbox/src/store.js
@@ -0,0 +1,54 @@
+/* A simple Pinia store/actions implementation.
+ * A true app would be more complex and separated into different files.
+ */
+import { defineStore } from 'pinia'
+
+/*
+ * The initial state of our store when the app loads.
+ * Usually, you would fetch this from a server. Let's not worry about that now
+ */
+const defaultTasks = [
+  { id: '1', title: 'Something', state: 'TASK_INBOX' },
+  { id: '2', title: 'Something more', state: 'TASK_INBOX' },
+  { id: '3', title: 'Something else', state: 'TASK_INBOX' },
+  { id: '4', title: 'Something again', state: 'TASK_INBOX' }
+]
+
+/*
+ * The store is created here.
+ * You can read more about Pinia defineStore in the docs:
+ * https://pinia.vuejs.org/core-concepts/
+ */
+export const useTaskStore = defineStore({
+  id: 'taskbox',
+  state: () => ({
+    tasks: defaultTasks,
+    status: 'idle',
+    error: null
+  }),
+  actions: {
+    // 将id对应的task的状态设置为已归档
+    archiveTask(id) {
+      const task = this.tasks.find((task) => task.id === id)
+      if (task) {
+        task.state = 'TASK_ARCHIVED'
+      }
+    },
+    // 将id对应的task的状态设置为置顶状态
+    pinTask(id) {
+      const task = this.tasks.find((task) => task.id === id)
+      if (task) {
+        task.state = 'TASK_PINNED'
+      }
+    }
+  },
+  getters: {
+    getFilteredTasks: (state) => {
+      const filteredTasks = state.tasks.filter(
+        (t) => t.state === 'TASK_INBOX' || t.state === 'TASK_PINNED'
+      )
+      // 过滤已归档的tasks，仅保留未归档的tasks
+      return filteredTasks
+    }
+  }
+})
